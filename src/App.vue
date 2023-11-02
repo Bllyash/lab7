de268d2056245543c683a694a4801aeef17fce2c
nbely <nbelyasnik20@gmail.com>
2023-11-02 12:20:21 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="page text-light">
+      <div class="container">
+        <div class="col-lg-6 offset-lg-3">
+          <regForm></regForm>
+        </div>
+      </div>
+      <regTable></regTable>
+      <MainFooter></MainFooter>
+    </div>
+</template>
+
+<style>
+html body{
+  background-color: #343a40;
+}
+</style>
+<script>
+import regForm from './components/regForm.vue';
+import regTable from './components/regTable.vue';
+import MainFooter from './components/MainFooter.vue';
+
+export default {
+  components: {regForm, regTable, MainFooter}
+}
+</script>
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer>
+    <div class="text-center m-5 ">
+      <h3>Фітнес центр "Дупа-яйця"</h3>
+      <p>Adress: Вулиця Гарна 14/88</p>
+    </div>
+  </footer>
+</template>
